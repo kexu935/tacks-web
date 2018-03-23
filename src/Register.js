89d00ecf14426ba9c60c26bc0700e16b3a08ff3b
kexu935 <xukekekekeke@gmail.com>
2018-03-22 21:17:16 -0700

    adjust to center

--- src/Register.js
+++ src/Register.js
@@ -63,7 +63,7 @@ class RegistrationForm extends React.Component {
         };
 
         return (
-            <Form onSubmit={this.handleSubmit}>
+            <Form onSubmit={this.handleSubmit} className="register-form">
                 <FormItem
                     {...formItemLayout}
                     label="Username"
