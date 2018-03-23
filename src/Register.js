108296b686cea2659332d536c4bcfbcab3c3933e
kexu935 <xukekekekeke@gmail.com>
2018-03-22 21:47:59 -0700

    add ajax

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message} from 'antd';
+import $ from 'jquery';
+import {API_ROOT} from './constants'
 
 const FormItem = Form.Item;
-
 
 class RegistrationForm extends React.Component {
     state = {
@@ -14,6 +15,20 @@ class RegistrationForm extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/signup`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password
+                    })
+                }).then((response) => {
+                    message.success(response);
+                    }, (response) => {
+                    message.error(response.responseText);
+                    }).catch((error) => {
+                        console.log(error);
+                    });
             }
         });
     }
