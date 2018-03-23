53259dff3c96e9fd939f0e61bab247073fdb1f36
kexu935 <xukekekekeke@gmail.com>
2018-03-22 20:19:01 -0700

    modify header

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,6 @@ export class Header extends React.Component {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
             </header>
         );
     }
