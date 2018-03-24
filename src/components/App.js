6a0271d4d6799258d97549d2e79d7555b5fc9ff1
kexu935 <xukekekekeke@gmail.com>
2018-03-24 14:12:34 -0700

    reorganize directory

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Header} from './Header'
 import {Main} from './Main'
 
-import './App.css';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
