50214234d1897de65774dd1c8b4e1a5c6ffab6ce
kexu935 <xukekekekeke@gmail.com>
2018-03-24 16:23:38 -0700

    add persistense

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Register } from './Register'
 import { Login } from './Login'
-import { Switch, Route } from 'react-router'
+import { Switch, Route, Redirect} from 'react-router'
 import { Home } from './Home'
+
 
 export class Main extends React.Component {
     getLogin = () => {
-        return this.props.isLoggedIn ? <Home/> : <Login handleLogin={this.props.handleLogin}/>
+        return this.props.isLoggedIn ? <Redirect to='/home'/> : <Login handleLogin={this.props.handleLogin}/>;
+    }
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to='/login'/>;
+    }
+    getRoot = () => {
+        return <Redirect to='/login'/>;
     }
     render() {
         return (
             <div className="main">
                 <Switch>
-                    <Route exact path="/" render={this.getLogin}/>
+                    <Route exact path="/" render={this.getRoot}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getRoot}/>
                 </Switch>
             </div>
         );
