02f77f3b0e1a0ae6c0be21dce9f90839bbb2fc47
kexu935 <xukekekekeke@gmail.com>
2018-03-30 00:02:21 -0700

    add home tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+const TabPane = Tabs.TabPane;
 
 export class Home extends React.Component {
     render() {
+        const operations = <Button type="primary">Create New Post</Button>;
         return (
-            <div>home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Map" key="2">Content of tab 2</TabPane>
+            </Tabs>
         );
     }
 }
