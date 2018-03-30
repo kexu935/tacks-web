b37bc77f57bcc59e08c0b96c562314556177cc2a
kexu935 <xukekekekeke@gmail.com>
2018-03-30 00:58:24 -0700

    set spin logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ const TabPane = Tabs.TabPane;
 export class Home extends React.Component {
     state = {
         loadingGeoLocation : false,
+        error: '',
     }
     getGeoLocation = () => {
         if ("geolocation" in navigator) {
@@ -27,7 +28,7 @@ export class Home extends React.Component {
     }
 
     onFailedLoadGeoLocation = () => {
-        this.setState({loadingGeoLocation:false});
+        this.setState({loadingGeoLocation:true, error:'Failed to load geo location!'});
         console.log('failed');
     }
 
@@ -36,12 +37,23 @@ export class Home extends React.Component {
         this.getGeoLocation();
     }
 
+    getGalleryPanelContent = () => {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>;
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip='Loading geo location...'/>;
+        } else {
+            return <div>content</div>
+        }
+    }
+
+
     render() {
         const operations = <Button type="primary">Create New Post</Button>;
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Posts" key="1">
-                    {this.state.loadingGeoLocation ? <Spin tip="Loading geo location..."/> : null}</TabPane>
+                    {this.getGalleryPanelContent()}</TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
             </Tabs>
         );
