02ad882bd8fd9e545d9a008dd56525e2eb084377
kexu935 <xukekekekeke@gmail.com>
2018-04-07 02:38:20 -0700

    handle drag

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -5,14 +5,26 @@ import { POS_KEY } from "../constants"
 
 
 class AroundMap extends React.Component {
+    reloadMarkers = () => {
+        const center = this.map.getCenter();
+        const position = {lat: center.lat(), lon: center.lng()};
+        this.props.loadNearbyPosts(position);
+    }
+
+    getMapRef = (map) => {
+        this.map=map;
+    }
 
     render() {
         const pos = JSON.parse(localStorage.getItem(POS_KEY));
         return (
             <GoogleMap
+                ref={this.getMapRef}
+                onDragEnd={this.reloadMarkers}
                 defaultZoom={8}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}>
-                {this.props.posts.map((post) => <AroundMarker key={`${post.url}`} post={post}/>)}
+                {this.props.posts ? this.props.posts.map((post) => <AroundMarker key={`${post.url}`} post={post}/>)
+                    : null}
             </GoogleMap>
         );
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -93,6 +93,7 @@ export class Home extends React.Component {
                     {this.getGalleryPanelContent()}</TabPane>
                 <TabPane tab="Map" key="2">
                     <WrappedAroundMap
+                        loadNearbyPosts={this.loadNearbyPosts}
                         posts={this.state.posts}
                         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
                         loadingElement={<div style={{ height: `100%` }} />}
