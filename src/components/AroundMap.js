d647f0b466c76291c513d2fdf856e22b5e9a34dd
kexu935 <xukekekekeke@gmail.com>
2018-04-14 19:26:16 -0700

    build

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 import { AroundMarker } from "./AroundMarker"
-import {METER_MILE, POS_KEY} from "../constants"
+import {M_KM, POS_KEY} from "../constants"
 
 
 class AroundMap extends React.Component {
@@ -23,7 +23,7 @@ class AroundMap extends React.Component {
         if (center && bounds) {
             const ne = bounds.getNorthEast();
             const right = new google.maps.LatLng(center.lat(), ne.lng());
-            return METER_MILE * google.maps.geometry.spherical.computeDistanceBetween(center, right);
+            return M_KM * google.maps.geometry.spherical.computeDistanceBetween(center, right);
         }
     }
 
